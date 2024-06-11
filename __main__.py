a7cf4830842ef508232a5f466b73126a46cec513
Drew Pihera <dpihera@gmail.com>
2024-06-11 10:50:30 -0400

    added datalakehouse creation

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@
 import pulumi
 import pulumi_aws as aws
 
+from capeinfra.datalake.datalake import DatalakeHouse
 from capeinfra.meta.capemeta import CapeMeta
 
 # get the stack name once since it's a function and we'll use this a bunch to
@@ -10,3 +11,4 @@ from capeinfra.meta.capemeta import CapeMeta
 CAPE_STACK_NS = pulumi.get_stack()
 
 cape_meta = CapeMeta(f"{CAPE_STACK_NS}-meta")
+datalake_house = DatalakeHouse(f"{CAPE_STACK_NS}-datalakehouse")
