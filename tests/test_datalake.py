bd6553033ca5ad99f571971c7b87ab56b9b5aa37
Micah Halter <micah.halter@gtri.gatech.edu>
2024-06-13 14:43:04 -0400

    test: add example test for datalake catalog

---
+++ tests/test_datalake.py
@@ -0,0 +1,16 @@
+import pulumi
+import pulumi_aws as aws
+
+
+@pulumi.runtime.test
+def test_catalog(mock_datalake):
+    def check_catalog(args):
+        catalog_database, catalog_name = args
+
+        assert catalog_name == f"{mock_datalake.name}-catalog"
+        assert type(catalog_database) == aws.glue.CatalogDatabase
+
+    return pulumi.Output.all(
+        mock_datalake.catalog.catalog_database,
+        mock_datalake.catalog.name,
+    ).apply(check_catalog)
