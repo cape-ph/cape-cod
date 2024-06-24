18710266a9a8d4ec6f07ac423446eb2ba4418c14
Drew Pihera <dpihera@gmail.com>
2024-06-24 16:19:35 -0400

    added ComponentResource subclass that takes a descriptive name

---
+++ capeinfra/pulumi.py
@@ -0,0 +1,20 @@
+"""Module of subclasses of pulumi objects."""
+
+from pulumi import ComponentResource
+
+
+class DescribedComponentResource(ComponentResource):
+    """Extension of ComponentResource that takes a descriptive name."""
+
+    def __init__(self, *args, desc_name=None, **kwargs):
+        """Constructor.
+
+        Takes all the same args/kwargs as pulumi's ComponentResource in addition
+        to the ones below.
+
+        Args:
+            desc_name: A descriptive name that can be added to tags for child
+                       components because names are so restricted in length.
+        """
+        super().__init__(*args, **kwargs)
+        self.desc_name = desc_name
