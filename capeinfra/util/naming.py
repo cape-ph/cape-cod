a07497dabb72749e1f2c7a9f0bb031ea86793baa
Drew Pihera <dpihera@gmail.com>
2024-08-07 12:46:23 -0400

    added module of utility functions for resource naming

---
+++ capeinfra/util/naming.py
@@ -0,0 +1,19 @@
+"""Module of naming utility functions."""
+
+# translation table of vowels for removal from strings
+VOWELS_TBL = str.maketrans(dict.fromkeys("aeiouAEIOU"))
+
+
+def disemvowel(s):
+    """Return a copy of the given string with vowels removed.
+
+    This is a naive method of shortening strings to deal with max string
+    lengths. May need a better plan in the future.
+
+    Args:
+        s: the string to remove vowels from.
+
+    Returns:
+        A copy of s with vowels removed.
+    """
+    return s.translate(VOWELS_TBL)
