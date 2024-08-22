f82968ecad538416f868cf3413e8480639f220eb
Drew Pihera <dpihera@gmail.com>
2024-08-22 14:00:25 -0400

    fixed logger.info, wired todo's to GH issues, removed obe comments

--- assets/lambda/sqs_dap_submit_lambda.py
+++ assets/lambda/sqs_dap_submit_lambda.py
@@ -7,16 +7,13 @@ import boto3
 
 from botocore.exceptions import ClientError
 logger = logging.getLogger(__name__)
+# TODO: ISSUE #84
 
-# TODO: need to be able to parameterize the region for our whole cape setup...
+# TODO: ISSUE #87
 ddb_resource = boto3.resource("dynamodb", region_name="us-east-2")
 
 
-# TODO: this function appears with the same name in multiple modules as well. 
-#       And we will likely have a similar function in any lambda that is 
-#       tasked with handling a ClientError. We should move this to a common 
-#       place (which is a little complicated as we would need to ensure the 
-#       common module is deployed to different lambda environments)
+# TODO: ISSUE #86
 def decode_error(err: ClientError):
     """Decode a ClientError from AWS.
 
@@ -35,12 +32,7 @@ def decode_error(err: ClientError):
             message = error["Message"]
     return code, message
 
-# TODO: this function appears with a different name in the
-#       queue_analysis_pipeline_run.py module as well. And we will likely
-#       have a similar function in any lambda that is tasked with reading from
-#       dynamodb table. We should move this to a common place (which is a
-#       little complicated as we would need to ensure the common module is
-#       deployed to different lambda environments)
+# TODO: ISSUE #86
 def get_dap_registry_table(table_name: str):
     """Get the DAP registry DynamoDB table by name.
 
@@ -75,15 +67,7 @@ def get_dap_registry_table(table_name: str):
 
     return table
 
-# TODO: we have a very similar function in the
-#       new_s3obj_queue_notifier_lambda.py module. This really boils down to
-#       "given a table name and a specific set of filters, find me the object
-#       in dynamo and handle client errors during". We could make the (in this
-#       case) `pipeline_name` and `pipeline_version` parameters into a dict of 
-#       dynamo keys/values and genricize this function to make it reusable. We 
-#       should move this to a common place (which is a little complicated as we
-#       would need to ensure the common module is deployed to different lambda 
-#       environments)
+# TODO: ISSUE #86
 def get_dap_registry_entry(table, pipeline_name: str, pipeline_version: str|None) -> dict | None:
     """Get the DAP registry entry for the given pipeline name from DynamoDB.
 
@@ -104,12 +88,9 @@ def get_dap_registry_entry(table, pipeline_name: str, pipeline_version: str|None
     try:
         k = {"pipeline_name": pipeline_name}
 
-        # TODO: if no version is specified, the expectation is probably to use
-        #       the latest. that implies we need some way to tag registry
-        #       entries as `latest` (and make it easy to not get into a
-        #       situation where 2 versions are marked latest)
+        # TODO: ISSUE #88
         if pipeline_version is not None:
-            k.update({"pipeline_version": pipeline_version})
+            k.update({"version": pipeline_version})
 
         response = table.get_item(
             Key=k
@@ -156,23 +137,19 @@ def index_handler(event, context):
         qmsg = json.loads(rec["body"])
         
         try:
-            # TODO: we need to figure out the actual set of params we'll need from
-            #       this endpoint. this will depend on what's needed to run the
-            #       actual pipeline.
+            # TODO: ISSUE #84
             pipeline_name = qmsg["pipeline_name"]
             pipeline_version = qmsg["pipeline_version"]
-            # TODO: these paths (if we keep using them) may be url encoded when we
-            #       actually get to more real stuff. not sure and could depend how 
-            #       we wire it up to UI.             
             input_path = qmsg["input_path"]
             output_path = qmsg["output_path"]
-           
 
             # attempt to get the registry entry from dynamodb. if we can't find
             # an entry, we'll log the error but not add to the batch failures
             # (if we can't find the entry, no amount of re-queing will help).
 
-            dap_reg_entry = get_dap_registry_entry(ddb_table, pipeline_name, pipeline_version)
+            dap_reg_entry = get_dap_registry_entry(
+                ddb_table, pipeline_name, pipeline_version
+            )
 
             if dap_reg_entry is None:
                 logger.error(
@@ -196,23 +173,13 @@ def index_handler(event, context):
                     "and will be sent to the appropriate head node."
                 )
 
-                # TODO: when we submit the job to the (correct) head node, 
-                #       hopefully there is some confirmation from the node (with a
-                #       run id or something) that we can store in 
+                # TODO: ISSUE #84 - when we submit the job to the (correct) head 
+                #       node, hopefully there is some confirmation from the node 
+                #       (with a run id or something) that we can store in 
                 #       successful_dap_jobs for our return 200 message. until
                 #       then we'll store a tuple of pipeline name/version just
                 #       so we can get a count
                 successful_dap_jobs.append((pipeline_name, pipeline_version))
-
-
-            # LEFTOFF: 
-            #  - wire up the sqs function 
-            #    - make perms for it to read the queue, read dynamo, and submit 
-            #      the jobs
-            #    - pass DAP_REG_DDB_TABLE as an env var
-
-
-
 
         except Exception as e:
             # We are going to requeue the message on *any* exception in
@@ -222,16 +189,7 @@ def index_handler(event, context):
                 f"Message will be requeued"
             )
 
-            # TODO: we may want to consider a dead letter queue to keep from
-            #       getting into a case where there are a ton of new 
-            #       submissions for a long period of time, which would likely 
-            #       outstrip our eventual gating of the number of pipelines 
-            #       that can run concurrently (right now we have no limit), 
-            #       which would lead to a number of re-queued messages that will
-            #       trigger lambda at the same time that a bunch of 
-            #       new submissions is also triggering lambda. can lead to a
-            #       "snowball" (anti)pattern. See here:
-            # https://docs.aws.amazon.com/prescriptive-guidance/latest/lambda-event-filtering-partial-batch-responses-for-sqs/best-practices-partial-batch-responses.html#snowball-anti-patterns
+            # TODO: ISSUE #89
 
             # we caught an exception that means we're not going to space today, but
             # could sometime in the future. so requeue the message hoping that day
