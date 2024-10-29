86db9d751b2fa131c50c9ca474de212c2191e474
Micah Halter <micah.halter@gtri.gatech.edu>
2024-10-29 09:49:58 -0400

    feat(meta): add `capepy` to `CapeMeta` on S3 and as a lambda layer

--- capeinfra/meta/capemeta.py
+++ capeinfra/meta/capemeta.py
@@ -1,6 +1,7 @@
 """Contains resources used by the whole CAPE infra deployment."""
 
-from pulumi import FileAsset, ResourceOptions
+import pulumi_aws as aws
+from pulumi import FileAsset, Output, ResourceOptions
 
 from ..resources.objectstorage import VersionedBucket
 from ..resources.pulumi import CapeComponentResource
@@ -32,10 +33,45 @@ class CapeMeta(CapeComponentResource):
                 source=FileAsset(etl_def["srcpth"]),
             )
 
+        self.capepy = CapePy(self.automation_assets_bucket)
+
         # We also need to register all the expected outputs for this component
         # resource that will get returned by default.
         self.register_outputs(
             {
                 "cape-meta-automation-assets-bucket": self.automation_assets_bucket.bucket
             }
+        )
+
+
+class CapePy(CapeComponentResource):
+    def __init__(self, assets_bucket: VersionedBucket, **kwargs):
+        self.name = "capepy"
+        super().__init__(
+            "capeinfra:meta:capemeta:CapePy",
+            self.name,
+            desc_name="Resources for distributing the CapePy library",
+            **kwargs,
+        )
+
+        self.bucket = assets_bucket
+        self.object = self.bucket.add_object(
+            f"{self.name}-object",
+            key="capepy.whl",
+            source=FileAsset("./assets/capepy-0.1.0-py3-none-any.whl"),
+        )
+
+        self.uri = Output.all(bucket=self.bucket, key=self.object).apply(
+            lambda args: f"s3://{args['bucket']}/{args['key']}"
+        )
+
+        self.lambda_layer = aws.lambda_.LayerVersion(
+            f"{self.name}-lmbd-lyr",
+            layer_name=self.name,
+            description="This layer provides the capepy Python library",
+            license_info=" Apache-2.0",
+            compatible_runtimes=["python3.11"],
+            s3_bucket=assets_bucket.bucket.bucket,
+            s3_key=self.object.key,
+            opts=ResourceOptions(parent=self),
         )
