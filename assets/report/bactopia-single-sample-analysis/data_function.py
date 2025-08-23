efcbc7a79937da5be4d6d9488385e9650168446f
Pihera <dpihera@gmail.com>
2025-08-23 13:46:09 -0400

    beginnings of report templates and data functions.

---
+++ assets/report/bactopia-single-sample-analysis/data_function.py
@@ -0,0 +1,140 @@
+# TODO:
+# need a layer with weasyprint and jinja2
+# - need this to be a lambda that returns (for now) this fake data.
+# - when we have the actual athena query, we will return data from the lake
+#   based on an incoming single sample analysis jobid
+import datetime
+
+import jinja2
+import weasyprint
+
+# TODO:
+
+# - need this to be a lambda that returns (for now) this fake data.
+# - when we have the actual athena query, we will return data from the lake
+#   based on an incoming single sample analysis jobid
+
+template_data = {
+    "sample_id": "0123456789",
+    "sample_type": "Environmental",
+    "sample_matrix": "Soil",
+    "sample_collection_date": (
+        datetime.datetime.fromisoformat(
+            "2025-08-15T14:36:28.024649+00:00"
+        ).strftime("%Y-%m-%d %H:%M %Z")
+    ),
+    "sample_collection_location": "Back yard",
+    "pipeline_meta": {
+        "name": "Bactopia",
+        "version": "3.0.2",
+        "run_date": (
+            datetime.datetime.fromisoformat(
+                "2025-08-15T16:22:33.024649+00:00"
+            ).strftime("%Y-%m-%d %H:%M %Z")
+        ),
+    },
+    "organisms_identified": [
+        {
+            "family": "Simpson",
+            "genus": "Bart",
+            "species": "Shorts",
+            "strain": "Eat My",
+        },
+        {
+            "family": "Griffin",
+            "genus": "Meg",
+            "species": "Hat",
+            "strain": "Fungus",
+        },
+        {
+            "family": "Smith",
+            "genus": "Morty",
+            "species": "Plumbus",
+            "strain": "Schmeckles",
+        },
+    ],
+    "virulence_info": {
+        "exact_protein_match": [
+            {
+                "element_symbol": "Pu",
+                "element_name": "Plutonium",
+                "scope": "micro",
+                "subtype": "sure?",
+                "class": "IO",
+                "subclass": "BytesIO",
+            },
+            {
+                "element_symbol": "Es",
+                "element_name": "Einsteinium",
+                "scope": "ortho",
+                "subtype": "no, thank you",
+                "class": "Exception",
+                "subclass": "OSError",
+            },
+        ],
+        "similarity_based_protein_match": [
+            {
+                "element_symbol": "Sr",
+                "element_name": "Strontium",
+                "scope": "stetho",
+                "subtype": "dr giggles",
+                "class": "List",
+                "subclass": "DoublyLinked",
+                "reference_coverage": "23",
+                "reference_identity": "10",
+            },
+            {
+                "element_symbol": "Fl",
+                "element_name": "Flerovium",
+                "scope": "oscillo",
+                "subtype": "yes",
+                "class": "object",
+                "subclass": "myobject",
+                "reference_coverage": "54",
+                "reference_identity": "41",
+            },
+        ],
+    },
+    "amr_info": {
+        "exact_protein_match": [
+            {
+                "element_symbol": "Sb",
+                "element_name": "Antimony",
+                "scope": "mouthwash",
+                "subtype": "non-alcoholic",
+                "class": "collection",
+                "subclass": "OrderedDict",
+            },
+            {
+                "element_symbol": "Fr",
+                "element_name": "Francium",
+                "scope": "endo",
+                "subtype": "tube",
+                "class": "Reader",
+                "subclass": "CsvReader",
+            },
+        ],
+        "similarity_based_protein_match": [
+            {
+                "element_symbol": "Xe",
+                "element_name": "Xenon",
+                "scope": "roto",
+                "subtype": "tolkien",
+                "class": "Head of the",
+                "subclass": "Arvid",
+                "reference_coverage": "90",
+                "reference_identity": "70",
+            },
+            {
+                "element_symbol": "Nh",
+                "element_name": "Nihonium",
+                "scope": "kaleido",
+                "subtype": "fractal",
+                "class": "stream",
+                "subclass": "iostream",
+                "reference_coverage": "87",
+                "reference_identity": "34",
+            },
+        ],
+    },
+}
